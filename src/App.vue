2e4d395b877920fe18de07ee3b65b7ca78823082
RainyDreams <cfzxycn@gmail.com>
2024-07-28 21:58:59 +0800

    优化移动端菜单动画时间

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
   <div class="page" :data-theme="isDarkMode?'dark':'light'" >
     <div class="mainpage">
       <div class="header" v-if="!TabBarHide">
-        <div :class="{navlist:1,show:showMenu}" :style="showMenu?'':'transition-delay: 0.10s;'">
+        <div :class="{navlist:1,show:showMenu}" :style="showMenu?'':'transition-delay: 0.20s;'">
           <a v-for="(item,i) in configList" :class="`nav ${activeName==item.name?'router-link-active':''} animate__animated ${M(showMenu?'animate__fadeInTopLeft':'animate__fadeOutTopLeft')}`" :style="(showMenu?`animation-duration:0.5s;`:'')+`animation-delay:${0.08*(i)}s`" :key="item.name" @click="isM(item.to,item.name)">
             <div class="icon" ><component :is="getIcon(item.icon)" theme="outline" size="22"/></div>
             <p>{{ item.title }}</p>
           </a>
           <div v-if="!TabBarHide" class="m tabbar" >
             <ul class="tablist">
-              <li :class="`tab ${(item.type=='primary')?'primary':''} animate__animated ${showMenu?'animate__fadeInLeft':'animate__fadeOutLeft'}`" :style="(showMenu?`animation-duration:0.6s;`:'')+`animation-delay:${0.2+0.08*(i)}s`" v-for="(item,i) in tabbarList" :key="i">
+              <li :class="`tab ${(item.type=='primary')?'primary':''} animate__animated ${showMenu?'animate__fadeInLeft':'animate__fadeOutLeft'}`" :style="(showMenu?`animation-duration:0.6s;`:'')+`animation-delay:${0.2+0.12*(i)}s`" v-for="(item,i) in tabbarList" :key="i">
                 <router-link class="" @click="toM(item.to)" :to="item.to" >
                   <div class="icon">
                     <component :is="getIcon(item.icon)" theme="outline" size="22"/>
