129bad0b487f1f06851698eb08c0f2b0c912121c
RainyDreams <cfzxycn@gmail.com>
2024-08-16 23:26:32 +0800

    更新信息

--- src/views/About/info.vue
+++ src/views/About/info.vue
@@ -10,7 +10,9 @@
       <p class="mb-2">开始时间：2024年7月3日</p>
       <p class="mb-2">项目官网：https://www.chiziingiin.top/project/RS2024/A</p>
       <p class="mb-2">团队成员：赤峰二中2023级12班：张新越、王严、宋鑫航、商航语、马世杰、姚懿轩（按照加入顺序排序）</p>
+      <p class="mb-2">指导教师：赤峰二中李智璁</p>
       <p class="my-6">赤子英金 · 只争朝夕</p>
+      <!-- <p class="my-6">零本智协 · </p> -->
     </div>
   </div>
 
