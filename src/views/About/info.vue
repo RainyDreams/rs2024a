b8e8323bf9f70838655541ad16f2497705232f5d
RainyDreams <cfzxycn@gmail.com>
2024-08-21 22:44:53 +0800

    更新软件信息

--- src/views/About/info.vue
+++ src/views/About/info.vue
@@ -3,7 +3,7 @@
   <div class="commonPage">
     <div class="panel text-base/snug">
       <p class="mb-6"><img style="height:60px;" src="https://lingben.chiziingiin.top/logo.webp" alt=""></p>
-      <p class="mb-2">软件临时代号：RS2024-A</p>
+      <p class="mb-2">软件代号：RS2024-A</p>
       <p class="mb-2">软件名称：零本智协</p>
       <p class="mb-2">软件版本：开发版 20240821-1</p>
       <p class="mb-2">Slogan：零成本构建创新高效应用程序</p>
@@ -11,9 +11,11 @@
       <p class="mb-2">项目官网：<a href="https://lingben.chiziingiin.top/">https://lingben.chiziingiin.top/</a></p>
       <p class="mb-2">团队成员：赤峰二中2023级12班：张新越、王严、宋鑫航、商航语、马世杰、姚懿轩（按照加入顺序排序）</p>
       <p class="mb-2">指导教师：赤峰二中李治璁</p>
-      <p class="my-6">赤子英金 · 只争朝夕</p>
-      <p>特别感谢：Qwen大模型、Cloudflare提供的免费服务</p>
-      <!-- <p class="my-6">零本智协 · </p> -->
+      <p class="mb-2 mt-4 text-lg"><b class="primary-text">「梦想可以大，第一步总是小的」</b></p>
+      <p class="mb-2">希望我们这样一款不成熟的微小产品可以给你带来一点点不一样的体验。</p>
+      <p class="mb-2">赤子英金 · 只争朝夕</p>
+      <p class="mb-2">特别感谢：Qwen大模型、Cloudflare提供的免费服务</p>
+      <p class="mb-2">2024年8月21日 雨窗</p>
     </div>
   </div>
 
