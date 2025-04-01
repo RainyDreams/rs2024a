28ea4f9f41ab59db6b55065a3e2af99d2c53385f
Xinyue Zhang (RainyDreams) <cfzxycn@gmail.com>
2025-04-01 23:47:26 +0800

    fix(Model): 优化聊天框滚动和加载动画
    - 调整聊天框滚动条样式，提升用户体验
    - 优化图片上传加载动画，增加条件渲染以提高性能

--- src/views/Model/chat.vue
+++ src/views/Model/chat.vue
@@ -12,7 +12,7 @@
     <div class="title text-center w-full text-xs pb-1 truncate px-5" >{{ title }}</div>
 
     <div 
-      :class="[`scroll active`,statusText?'will-change-auto':'']"
+      :class="[`scroll active`,statusText?'will-change-scroll':'']"
       @scroll.passive="checkScollStatus"
       @wheel.passive="onWheel"
       @touchstart.passive="onWheel"
@@ -398,7 +398,7 @@
     </div>
     <div :data-show="uploadPhotoDialogLoading" class="fixed flex justify-center items-center inset-0 bg-black bg-opacity-30 z-50 w-screen px-4 pt-4 pb-8 h-svh autohidden">
       <div class="p-6 bg-white rounded-3xl">
-        <svg class="animate-spin inline-block ml-1 mr-2 h-5 w-5 text-stone-500 " style="animation-duration:0.6s !important;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <svg v-if="uploadPhotoDialogLoading" class="animate-spin inline-block ml-1 mr-2 h-5 w-5 text-stone-500 " style="animation-duration:0.6s !important;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
           <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
           <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>上传中
