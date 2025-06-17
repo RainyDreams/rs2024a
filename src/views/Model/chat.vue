a5cff8c2cd51e10def2a2999cd2bee35ac656abf
Xinyue Zhang (RainyDreams) <cfzxycn@gmail.com>
2025-06-17 23:08:51 +0800

    refactor(Model): 更新聊天页面提示信息
    - 修改免费服务器性能提示信息，增加对不同模型的说明
    - 调整提示信息的样式，修改文本颜色为 slate-600
    - 注释掉一个空的 div 元素

--- src/views/Model/chat.vue
+++ src/views/Model/chat.vue
@@ -169,7 +169,8 @@
                 <div v-if="!welcome_loading && !loginStatus && chatList.length!=0" class="w-full">
                   <div class="text-sm text-slate-800 w-full text-center mt-4 lg:mt-8 opacity-80">未登录，正在以访客身份对话，对话不会被保留</div>
                 </div>
-                <div class="text-sm text-slate-800 w-full text-center mt-4 lg:mt-8 opacity-80">免费服务器性能调度有限，回复若延迟，见谅。我们在努力解决</div>
+                <div class="text-sm text-slate-600 w-full text-center mt-4 lg:mt-8 opacity-80">免费服务器性能调度有限，回复略有延迟，如果是lingben-dragon回复问题那么就是无阉割版，其他模型均为阉割版，敬请见谅。请留给我们进步的空间</div>
+                <!-- <div class="text-sm text-slate-600 w-full text-center mt-2 lg:mt-8 opacity-80"></div> -->
               </div>
                
               
